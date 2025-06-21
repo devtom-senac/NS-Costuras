bdcb74e00bf4a9b2e098f90a32e51acee80bddb5
Heverton Souza <heverton.nsouza@senacsp.edu.br>
2025-06-21 00:06:42 -0300

    Update Home.jsx

--- CorreDaQuebrada/nscosturas/src/components/Home.jsx
+++ CorreDaQuebrada/nscosturas/src/components/Home.jsx
@@ -12,10 +12,10 @@ function Home() {
       <div className="z-10 max-w-7xl w-full mx-auto flex flex-col md:flex-row items-center gap-12">
         <div className="flex-1 flex flex-col items-center">
           <h1 className=" md:text-6xl font-extrabold leading-tight mb-6 text-center text">
-            <span className="text-purple-400 text-8xl">Costura</span> <span className="text-8xl">que transforma</span>
+            <span className="text-purple-400 text-6xl">Costura</span> <span className="text-6xl">que transforma</span>
           </h1>
           {/* texto do parágrafo principal*/}
-          <p className="text-4xl md:text-4xl text-gray-300 mb-6 text-center">
+          <p className="text-2xl md:text-4xl text-gray-300 mb-6 text-center">
             Ajustes e consertos com excelência, cuidado e atenção aos detalhes.
           </p>
 
@@ -32,12 +32,12 @@ function Home() {
             wrapper="p"
             cursor={true}
             repeat={Infinity}
-            className="text-center text-5xl text-purple-300 font-medium mb-8 h-[3rem]"
+            className="text-center text-4xl text-purple-300 font-medium mb-8 h-[3rem]"
           />
 
           <a
             href="#contato"
-            className="inline-block text-2xl bt-10 bg-purple-800 hover:bg-purple-400 text-white font-semibold px-8 py-3 rounded-xl transition-all mt-15"
+            className="inline-block text-1xl bt-10 bg-purple-800 hover:bg-purple-400 text-white font-semibold px-8 py-3 rounded-xl transition-all mt-15"
           >
             Clique aqui e fale conosco
           </a>
